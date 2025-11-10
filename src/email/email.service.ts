8ca347dbf3f22c8f4c78a5778ea3774ad7c6e65c
Dawid Kwiaton <dawid.kwiaton@itcraft.pl>
2025-11-10 23:32:29 +0100

    update email to breevo

--- src/email/email.service.ts
+++ src/email/email.service.ts
@@ -7,9 +7,9 @@ export class EmailService {
 
   constructor() {
     this.transporter = nodemailer.createTransport({
-      host: process.env.SMTP_HOST, // np. smtp.gmail.com
+      host: process.env.SMTP_HOST,
       port: Number(process.env.SMTP_PORT) || 587,
-      secure: false,
+      secure: false, // TLS w Brevo na 587 nie wymaga secure
       auth: {
         user: process.env.SMTP_USER,
         pass: process.env.SMTP_PASS,
@@ -38,7 +38,7 @@ export class EmailService {
 
       await this.transporter.sendMail({
         from: `"Strona Rezerwacji" <${process.env.SMTP_USER}>`,
-        to: process.env.TARGET_EMAIL, // gdzie ma dojść mail
+        to: process.env.TARGET_EMAIL,
         subject: 'Nowa rezerwacja ze strony',
         html,
       });
