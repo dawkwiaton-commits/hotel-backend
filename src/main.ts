ee7d2288c9aee52eae648caa1c24ca4480e2d524
Dawid Kwiaton <dawid.kwiaton@itcraft.pl>
2025-11-10 23:20:40 +0100

    enable cors

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,14 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: [
+      'http://localhost:3000',
+      'https://hotel-front-git-main-dawkwiaton-commits-projects.vercel.app'
+    ],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+  });
+
   const PORT = process.env.PORT ? parseInt(process.env.PORT, 10) : 3001; // <- nowy port 3001
   await app.listen(PORT);
   console.log(`Server running on http://localhost:${PORT}`);
